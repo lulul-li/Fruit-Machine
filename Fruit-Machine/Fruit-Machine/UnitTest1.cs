5d71b91470c30a800bfc0e7f553881624e4da130
lulu.li29 <s987337613@gmail.com>
2017-12-03 23:46:24 +0800

    add test ThreeWild_Return_100

--- Fruit-Machine/Fruit-Machine/UnitTest1.cs
+++ Fruit-Machine/Fruit-Machine/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace Fruit_Machine
             Assert.AreEqual(0, Fruit(reels, spins));
         }
 
+        [Test]
+        public void ThreeWild_Return_100()
+        {
+            var reels = new List<string[]> {
+                new[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" },
+                new[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" },
+                new[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" }
+            };
+            var spins = new int[] { 0, 0, 0 };
+            Assert.AreEqual(100, Fruit(reels, spins));
+        }
+
         private int Fruit(List<string[]> reels, int[] spins)
         {
             return 0;
