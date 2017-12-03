ded520909d01208501becbda389722e10cc77df4
lulu.li29 <s987337613@gmail.com>
2017-12-03 23:40:49 +0800

    add test and create Fruit

---
+++ Fruit-Machine/Fruit-Machine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fruit-Machine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fruit-Machine")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5e91fad-18c1-419a-96b9-2a9ec3aad147")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Fruit-Machine/Fruit-Machine/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Fruit_Machine
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+            var reels = new List<string[]> { };
+            var spins=new int[]{};
+            Assert.AreEqual(0,Fruit(reels, spins));
+        }
+
+        private int Fruit(List<string[]> reels, int[] spins)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
